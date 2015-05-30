0b39a22c31f03502e92e6658a809559df656cbf3
Illia Klimov <illia.klimov@gmail.com>
2015-05-30 12:24:02 +0200

    Algorithms
    project structure

---
+++ src/com/etnos/algorithms/Main.java
@@ -0,0 +1,8 @@
+package com.etnos.algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/etnos/algorithms/ReversString.java
@@ -0,0 +1,15 @@
+package com.etnos.algorithms;
+
+
+public class ReversString {
+    public static void main(String[] args) {
+        ReversString reversString = new ReversString();
+        String testString = "12345";
+        String result = reversString.reverseString(testString);
+        System.out.println(result);
+    }
+
+    protected String reverseString(String str) {
+        return "e";
+    }
+}
