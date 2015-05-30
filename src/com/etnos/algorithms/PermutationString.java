b37b0eeb248dfe1aa37f34f79e6eca0fcc6d9313
Illia Klimov <illia.klimov@gmail.com>
2015-05-30 23:51:49 +0200

    PermutationString
    Done

--- src/com/etnos/algorithms/PermutationString.java
+++ src/com/etnos/algorithms/PermutationString.java
@@ -7,35 +7,32 @@ import java.util.Arrays;
 public class PermutationString {
     public static void main(String[] args) {
         PermutationString permutationString = new PermutationString();
-        String string = "abc";
-
-        ArrayList<String> permutationHolder = new ArrayList<>();
+        String string = "abcd";
 
         System.out.println("Current string: " + string);
 
-        System.out.println("loopPermutation: " + permutationString.loopPermutation(string));
-
-        permutationHolder = permutationString.recursivePermutation(string);
+        ArrayList<String> permutationHolder = permutationString.recursivePermutation(string);
 
         System.out.println("recursivePermutation: " + Arrays.toString(permutationHolder.toArray()));
-    }
-
-    protected String loopPermutation(String str) {
-        String result = "";
-        return result;
     }
 
     protected ArrayList<String> recursivePermutation(String str) {
         ArrayList<String> permList = new ArrayList<>();
         if (str != null && str.length() > 0) {
-            doPermutationRec(str, permList);
+            doPermutationRec(str, "", permList);
         }
         return permList;
     }
 
-    protected void doPermutationRec(String str, ArrayList<String> permList) {
-        if (str.length() > 0) {
-
+    protected void doPermutationRec(String defaultString, String permutationPrefix, ArrayList<String> permList) {
+        if (defaultString.length() > 0) {
+            for (int i = 0; i < defaultString.length(); i++) {
+                String newDefaultString = defaultString.substring(0, i) + defaultString.substring(i + 1, defaultString.length());
+                String newPermutationPrefix = permutationPrefix + defaultString.charAt(i);
+                doPermutationRec(newDefaultString, newPermutationPrefix, permList);
+            }
+        } else {
+            permList.add(permutationPrefix);
         }
     }
 }
