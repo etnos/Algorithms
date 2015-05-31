939f801639d60e182ac48a4858236ca3ae4fba6f
Illia Klimov <illia.klimov@gmail.com>
2015-05-31 12:41:01 +0200

    PermutationString
    Added comments

--- src/com/etnos/algorithms/PermutationString.java
+++ src/com/etnos/algorithms/PermutationString.java
@@ -3,19 +3,31 @@ package com.etnos.algorithms;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
+/**
+ * This java program makes a string permutation.
+ * <p>
+ * String "abc" -> [abc, acb, bac, bca, cab, cba]
+ */
 public class PermutationString {
     public static void main(String[] args) {
         PermutationString permutationString = new PermutationString();
-        String string = "abcd";
+        String string = "abc";
 
         System.out.println("Current string: " + string);
 
         ArrayList<String> permutationHolder = permutationString.recursivePermutation(string);
 
+        System.out.println("Permutation size: " + permutationHolder.size());
+
         System.out.println("recursivePermutation: " + Arrays.toString(permutationHolder.toArray()));
     }
 
+    /**
+     * Check a given string and call {@link #doPermutationRec(String, String, ArrayList)} to calculate permutation
+     *
+     * @param str a given sting for a permutation. can be null or empty
+     * @return list of permuted strings or empty list
+     */
     protected ArrayList<String> recursivePermutation(String str) {
         ArrayList<String> permList = new ArrayList<>();
         if (str != null && str.length() > 0) {
@@ -24,6 +36,13 @@ public class PermutationString {
         return permList;
     }
 
+    /**
+     * Process all permutation of a given string. Uses recursion to calculate all permutations
+     *
+     * @param defaultString     string with a given value
+     * @param permutationPrefix substring with prefix value for a permutation
+     * @param permList          placeholder for saving permutation results
+     */
     protected void doPermutationRec(String defaultString, String permutationPrefix, ArrayList<String> permList) {
         if (defaultString.length() > 0) {
             for (int i = 0; i < defaultString.length(); i++) {
